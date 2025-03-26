6b96275cb1803197e82e1fc42e07ae67f6bd5f43
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-26 19:00:32 -0300

    Tipos de eventos y callbacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 import './App.css'
 import { RandomFox } from './RandomFox'
 
@@ -8,22 +8,25 @@ const random = () => Math.floor(Math.random() * 123) + 1
 // Id random 
 const generateId = () => Math.random().toString(36).substr(2, 9)
 
-
 type ImageItems = {id: string, url: string}
 
 function App() {
 
-  const [images,setImages] = useState<Array<ImageItems>>([
-    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
-  ]);
+  const [images,setImages] = useState<Array<ImageItems>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    const newItem = {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`}
+    setImages([...images, newItem])
+  }
 
   return (
     <>
       <h1>Imagenes de zorros aleatorios</h1>
       <p>Actualiza la pagina para ver una nueva imagen!</p>
+
+    <button onClick={addNewFox}>Agregar un nuevo zorro</button>
+
       {images.map(({id, url}) => (
           <div className="p-4" key={id}>
             <RandomFox image={url} />
