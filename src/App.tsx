2b44c4da5e50b7c1879bd801a9a07e3770f2e397
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-26 18:18:28 -0300

    State personalizado

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import { RandomFox } from './RandomFox'
 
-  // Random para obtener zorros aleatorios entre 1 y 123
-  const random = () => Math.floor(Math.random() * 123) + 1
+// Random para obtener zorros aleatorios entre 1 y 123
+const random = () => Math.floor(Math.random() * 123) + 1
+
+// Id random 
+const generateId = () => Math.random().toString(36).substr(2, 9)
+
+
+type ImageItems = {id: string, url: string}
 
 function App() {
 
-  const [images, setImages] = useState<Array<string>>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images,setImages] = useState<Array<ImageItems>>([
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
   ]);
 
   return (
     <>
       <h1>Imagenes de zorros aleatorios</h1>
       <p>Actualiza la pagina para ver una nueva imagen!</p>
-      {/* <RandomFox image ={}/> */}
-      {images.map((image, index) => (
-          <div className="p-4" key={index}>
-            <RandomFox image={image} />
+      {images.map(({id, url}) => (
+          <div className="p-4" key={id}>
+            <RandomFox image={url} />
           </div>
         ))}
     </>
