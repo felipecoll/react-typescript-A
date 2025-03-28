94e95f92cff085562aa23f2f0c25939c016bb3d0
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-28 17:43:37 -0300

    Lazy loading con observadores

--- src/RandomFox.tsx
+++ src/RandomFox.tsx
@@ -1,16 +1,42 @@
-import { useRef } from "react"
+import { useRef, useEffect, useState } from "react"
 
 type Props = {image: string}
 
 
 export const RandomFox = ({image}: Props) => {
+
     const node = useRef<HTMLImageElement>(null)
-
-    // Lazy loading hara uso en esta seccion
-
-  return (
-    <>
-        <img ref={node} width={320} height='auto' className='roundend' src={ image} alt='Zorritos meme' />
-    </>
-  )
-}
+    const [src, setSrc] = useState(
+      "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+    );
+  
+    useEffect(() => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting || !node.current) {
+            return;
+          }
+  
+          setSrc(image);
+        });
+      });
+  
+      if (node.current) {
+        observer.observe(node.current);
+      }
+  
+      return () => {
+        observer.disconnect();
+      };
+    }, [image]);
+  
+    return (
+      <img
+        ref={node}
+        width="320"
+        height="auto"
+        src={src}
+        className="mx-auto rounded-md bg-gray-300"
+      />
+    );
+  }
